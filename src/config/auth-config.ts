813f51959edea1b08d4f311ef1903a79d271722e
Frank <frankbpr@amazon.com>
2025-04-19 22:12:37 -0500

    working cognito UI with domain

---
+++ src/config/auth-config.ts
@@ -0,0 +1,33 @@
+import { ResourcesConfig } from 'aws-amplify';
+
+const requireEnvVar = (name: string): string => {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing required environment variable: ${name}`);
+  }
+  return value;
+};
+
+export const authConfig: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: requireEnvVar('REACT_APP_USER_POOL_ID'),
+      userPoolClientId: requireEnvVar('REACT_APP_USER_POOL_CLIENT_ID'),
+      loginWith: {
+        oauth: {
+          domain: requireEnvVar ('REACT_APP_COGNITO_DOMAIN'),
+          scopes: ['email', 'openid', 'profile'],
+          redirectSignIn: ['http://localhost:3000',
+            'https://main.d3lg2e3qgeamfr.amplifyapp.com/',
+             'https://cognito.funtamentals.online'
+          ],
+          redirectSignOut: ['http://localhost:3000',
+            'https://main.d3lg2e3qgeamfr.amplifyapp.com/',
+            'https://cognito.funtamentals.online'
+          ],
+          responseType: 'code'
+        }
+      }
+    }
+  }
+};
